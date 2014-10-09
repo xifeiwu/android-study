75f42da1c1b11178cf5c3b8e0804a793766a0ae9
xifei <xifei@nfs.iscas.ac.cn>
2014-10-09 18:21:13 +0800

    init repo AndroidStudy

---
+++ src/study/android/activity/MainActivity.java
@@ -0,0 +1,72 @@
+package study.android.activity;
+
+import java.util.logging.Logger;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        activityStates("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        activityStates("onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        // TODO Auto-generated method stub
+        super.onRestart();
+        activityStates("onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        activityStates("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        activityStates("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        super.onStop();
+        activityStates("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        activityStates("onDestroy");
+    }
+
+    private Logger logger = Logger.getLogger(MainActivity.class.getName());
+    private void activityStates(String state){
+        Toast.makeText(this, state, Toast.LENGTH_LONG).show();
+        logger.info(state);
+    }
+    
+    private void startWebviewActivity(){
+        Intent intent = new Intent(this, WebViewActivity.class);
+        startActivity(intent);
+    }
+}
