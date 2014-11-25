c1806ec72db0c870fc8d5af130f627a72ed1627e
xifei <xifei@nfs.iscas.ac.cn>
2014-11-25 12:36:35 +0800

    change button to ListView in MainActivity

--- src/study/android/activity/MainActivity.java
+++ src/study/android/activity/MainActivity.java
@@ -6,6 +6,7 @@ import org.apache.cordova.TestCordovaActivity;
 
 import study.android.content.Intent.StartActivityListView;
 import study.android.content.Intent.StartOtherAppsByIntent;
+import study.android.content.Intent.StartActivityListView.ToDo;
 import study.android.content.pm.PMActivity;
 import study.android.content.pm.PMActivity_ListView;
 import study.android.surfaceview.SurfaceViewActivity;
@@ -15,19 +16,33 @@ import study.android.widget.ListViewActivity;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity{
     public static String TAG = "StudyAndroid";
+    private ListView activityList;
+    private MainActivity mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main);
+        activityList = (ListView) this.findViewById(R.id.listview);
+        activityList.setAdapter(new arrayAdapter(this, R.layout.main_item, activityArray));
+        activityList.setOnItemClickListener(itmeClickListener);
+        mContext = this;
         LOG.setTAG(TAG);
         LOG.setLogLevel(LOG.INFO);
         LOG.i(TAG, "onCreate");
@@ -81,44 +96,137 @@ public class MainActivity extends Activity{
         Toast.makeText(this, MainActivity.class.getName() + " - " + state, Toast.LENGTH_SHORT).show();
         logger.info(state);
     }
-    
-    public void startWebviewActivity(View view){
-        Intent intent = new Intent(this, WebViewActivity.class);
-        startActivity(intent);
+
+    public static enum ToDo {
+        WEBVIEWACTIVITY,
+        PACKAGEMANAGERACTIVITY,
+        HANDLERACITIV,
+        SURFACEVIEWACTIVITY,
+        LISTVIEWACTIVITY,
+        TESTCORDOVAACTIVITY,
+        TESTWEBVIEWCLIENT,
+        STARTOTHERAPPSBYINTENT,
+    };
+
+    private ActivityInfo[] activityArray = new ActivityInfo[]{
+            new ActivityInfo(
+                    ToDo.WEBVIEWACTIVITY.ordinal(),
+                    "WebviewActivity", 
+                    "Webview控件使用Demo。"),
+            new ActivityInfo(
+                    ToDo.PACKAGEMANAGERACTIVITY.ordinal(), 
+                    "PackageManagerActivity", 
+                    "PackageManager控件使用Demo。"),
+            new ActivityInfo(
+                    ToDo.HANDLERACITIV.ordinal(),
+                    "tHandlerActivity", 
+                    "Handler控件使用Demo。"),
+            new ActivityInfo(
+                    ToDo.SURFACEVIEWACTIVITY.ordinal(),
+                    "SurfaceViewActivity", 
+                    "SurfaceView控件使用Demo。"),
+            new ActivityInfo(
+                    ToDo.LISTVIEWACTIVITY.ordinal(),
+                    "ListViewActivity", 
+                    "ListView控件使用Demo。"),
+            new ActivityInfo(
+                    ToDo.TESTCORDOVAACTIVITY.ordinal(),
+                    "TestCordovaActivity", 
+                    "学习Cordova中的方法"),
+            new ActivityInfo(
+                    ToDo.TESTWEBVIEWCLIENT.ordinal(),
+                    "TestWebViewClient", 
+                    "WebViewClient控件使用Demo。"),
+            new ActivityInfo(
+                    ToDo.STARTOTHERAPPSBYINTENT.ordinal(),
+                    "StartOtherAppsByIntent", 
+                    "Intent控件使用Demo。"),
+    };
+    private OnItemClickListener itmeClickListener = new OnItemClickListener(){
+
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            // TODO Auto-generated method stub
+            ToDo todo = ToDo.values()[arg2]; //do your own bounds checking
+            Intent intent = null;
+            switch(todo){
+            case WEBVIEWACTIVITY:
+                intent = new Intent(mContext, WebViewActivity.class);
+                break;
+            case PACKAGEMANAGERACTIVITY:
+                intent = new Intent(mContext, PMActivity_ListView.class);
+                break;
+            case HANDLERACITIV:
+                intent = new Intent(mContext, HandlerActivity.class);
+                break;
+            case SURFACEVIEWACTIVITY:
+                intent = new Intent(mContext, SurfaceViewActivity.class);
+                break;
+            case LISTVIEWACTIVITY:
+                intent = new Intent(mContext, ListViewActivity.class);
+                break;
+            case TESTCORDOVAACTIVITY:
+                intent = new Intent(mContext, TestCordovaActivity.class);
+                break;
+            case TESTWEBVIEWCLIENT:
+                intent = new Intent(mContext, TestWebViewClient.class);
+                break;
+            case STARTOTHERAPPSBYINTENT:
+                intent = new Intent(mContext, StartOtherAppsByIntent.class);
+                break;
+            }
+            if(null != intent){
+                startActivity(intent);
+            }
+        }   
+    };
+}
+
+class arrayAdapter extends ArrayAdapter<ActivityInfo>{
+    private Context mContext;
+    private int resourceId;
+    private ActivityInfo[] adapter;
+
+    public arrayAdapter(Context context, int resource, ActivityInfo[] objects) {
+        super(context, resource, objects);
+        // TODO Auto-generated constructor stub
+        mContext = context;
+        resourceId = resource;
+        adapter = objects;
     }
-    
-    public void startPackageManagerActivity(View view){
-        Intent intent = new Intent(this, PMActivity_ListView.class);
-        startActivity(intent);
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        if(convertView==null){
+            // inflate the layout
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(resourceId, null);
+        }
+        TextView textViewName = (TextView) convertView.findViewById(R.id.name);
+        textViewName.setText(adapter[position].getName());
+        TextView textViewDetail = (TextView) convertView.findViewById(R.id.detail);
+        textViewDetail.setText(adapter[position].getDetail());
+        return convertView;
+    }    
+}
+
+class ActivityInfo{
+    private int id;
+    private String name;
+    private String detail;
+    public ActivityInfo(int id, String name, String detail){
+        this.id = id;
+        this.name = name;
+        this.detail = detail;
     }
-    
-    public void startHandlerActivity(View view){
-        Intent intent = new Intent(this, HandlerActivity.class);
-        startActivity(intent);
+    public int getId(){
+        return id;
     }
-    
-    public void startSurfaceViewActivity(View view){
-        Intent intent = new Intent(this, SurfaceViewActivity.class);
-        startActivity(intent);
+    public String getName(){
+        return name;
     }
-    
-    public void startListViewActivity(View view){
-        Intent intent = new Intent(this, ListViewActivity.class);
-        startActivity(intent);
-    }
-    
-    public void startTestCordovaActivity(View view){
-        Intent intent = new Intent(this, TestCordovaActivity.class);
-        startActivity(intent);
-    }
-    
-    public void startTestWebViewClient(View view){
-        Intent intent = new Intent(this, TestWebViewClient.class);
-        startActivity(intent);
-    }
-    
-    public void startStartOtherAppsByIntent(View view){
-        Intent intent = new Intent(this, StartOtherAppsByIntent.class);
-        startActivity(intent);
+    public String getDetail(){
+        return detail;
     }
 }
