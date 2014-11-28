7a4f2a5c4b32b4846f3877613b276908b8769056
xifei <xifei@nfs.iscas.ac.cn>
2014-11-28 15:25:09 +0800

    study.html ok

--- assets/appframework/custom.js
+++ assets/appframework/custom.js
@@ -1,3 +1,18 @@
 function updateLogView(){
-    $('#afui .panel #panel_nsd_logview_form textarea').val("hello");
+    // var panel_nsd_logview = $('#afui #content #panel_nsd_logview');
+    // var panel_nsd_logview_form = $(panel_nsd_logview).find('#panel_nsd_logview_form');
+    // var panel_nsd_logview_form_textarea = $(panel_nsd_logview_form).find('textarea');
+    var panel_nsd_talk_history = $('#afui #content #panel_nsd_talk').find('ul');
+    var panel_nsd_talk_footer = $('#afui #panel_nsd_talk_footer').find('textarea');
+    var content = panel_nsd_talk_footer.val();
+    if(content.length){
+        panel_nsd_talk_history.append($('<li></li>').html(content));
+        panel_nsd_talk_footer.val('');
+    }else{
+        alert("内容不能为空");
+    }
+    // console.log("content: " + content);
+    // panel_nsd_logview_form_textarea
+    // $('#afui #content #panel_nsd_logview #panel_nsd_logview_form textarea').val("hello");
+    // $(panel_nsd_logview_form).children('textarea').val('hellofdasf.');
 }
